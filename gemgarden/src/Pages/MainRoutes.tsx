be85e49f92d115145536cb1c2db717f1d89c4964
sharvarihupare-369 <sharvarihupare15@gmail.com>
2023-06-13 17:30:20 +0530

    boilerplate

--- gemgarden/src/Pages/MainRoutes.tsx
+++ gemgarden/src/Pages/MainRoutes.tsx
@@ -1,7 +1,35 @@
+import { Box } from '@chakra-ui/react'
 import React from 'react'
+import {Routes,Route} from 'react-router-dom'
+import { HomePage } from './HomePage'
+import { Admin } from './Admin'
+import { Login } from './Login'
+import { SignUp } from './SignUp'
+import { Product } from './Product'
+import { Contact } from './Contact'
+import { About } from './About'
+import { SingleProductPage } from './SingleProductPage'
+import { Cart } from './Cart'
+import { Payment } from './Payment'
+import { WishList } from './WishList'
 
 export const MainRoutes = () => {
   return (
-    <div>MainRoutes</div>
+   <Box>
+    <Routes>
+      <Route path='/' element={<HomePage/>} />
+      <Route path='/contact' element={<Contact/>} />
+      <Route path='/about' element={<About/>} />
+      <Route path='/admin' element={<Admin/>} />
+      <Route path='/login' element={<Login/>} />
+      <Route path='/signup' element={<SignUp/>} />
+      <Route path='/product' element={<Product/>} />
+      <Route path='/product/:id' element={<SingleProductPage/>} />
+      <Route path='/cart' element={<Cart/>} />
+      <Route path='/wishlist' element={<WishList/>} />
+      <Route path='/payment' element={<Payment/>} />
+
+    </Routes>
+   </Box>
   )
 }
